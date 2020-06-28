afefe4c42a2222b31a4b7842b2986196f758b5df
adedaniel <adetoladaniel693@gmail.com>
2020-06-28 16:57:55 +0100

    Implement Job Tweets display

---
+++ src/pages/tweets.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Appbar from "../components/appbar";
+import TweetsComponent from "../features/tweets";
+import Head from "next/head";
+import { Box } from "@chakra-ui/core";
+
+function Tweets() {
+  return (
+    <>
+      <Head>
+        <title>See Jobs | Job Tweets</title>
+      </Head>
+      <Box backgroundColor="gray.50" pb={10}>
+        <Appbar />
+        <TweetsComponent />
+      </Box>
+    </>
+  );
+}
+
+export default Tweets;
